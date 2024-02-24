531638bce2a11f2369701334120c9ffaaf5087df
mich_iv <michelle.ivgo@gmail.com>
2024-02-23 23:27:39 -0800

    primero

---
+++ pruebaSitio/src/menu.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import Menu from './Components/Menu.jsx'
+
+ReactDOM.createRoot(document.getElementById('menu')).render(
+  <React.StrictMode>
+    <Menu/>
+  </React.StrictMode>,
+)

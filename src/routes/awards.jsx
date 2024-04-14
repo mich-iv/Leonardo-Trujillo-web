32639136d7316221f3965a4f01f2eae97ba917fd
mich_iv <michelle.ivgo@gmail.com>
2024-04-13 19:48:44 -0700

    Estilos y editor de contenido

--- src/routes/awards.jsx
+++ src/routes/awards.jsx
@@ -1,41 +1,52 @@
 import React, { useEffect, useState } from 'react'
 import '../estilos/Paginas.css';
 import { Employees } from "../bd/datos.json";
-import { useLoaderData } from 'react-router-dom';
+import { useLoaderData, useLocation } from 'react-router-dom';
 import {bd, collection, getDocs, doc, getDoc} from '../../firebase.jsx';
 
-const querySnapshot = await getDocs(collection(bd, "awards"));
-querySnapshot.forEach((docu) => {
-    console.log(`${docu.id} => ${docu.data()}`);
-});
-
 export default function Route(){
+    var prueba;
+    const location = useLocation();
+    const [textoCargado, setTextoCargado]= useState("");
+
+    const mostrarTexto = () => {
+        return <div dangerouslySetInnerHTML={{ __html: textoCargado }} />;
+    };
+
+    useEffect (() => {
+        async function docSnap(){
+            // const response = await firebase.firestore().collection('awards').get();
+            var response = await getDocs(collection(bd, location.pathname));
+            const docs = response.docs.map((doc) => {
+                const data = doc.data();
+                data.id = doc.id;
+                return data;
+            })
+            return docs;
+        }
+        docSnap().then(valor => {
+            prueba = valor;
+            setTextoCargado(prueba[0].texto);
+        });
+
+    }, []);
+
     return(
         <div>
             <div>
-                    <h1 className='titulos'>
-                        Awards
-                    </h1>
+                <h1 className='titulos'>
+                    Awards
+                </h1>
 
-                    
+                
 
-                    {/* {Employees.map((data, key) => {
-                        return <h2 key={key}>{data.preferredFullName}</h2>;
-                    })
-                    } */}
-                    <div  className='texto'>
-                        <p>Nombre: {Employees[0].preferredFullName}</p>
-                        <p>Correo: {Employees[0].emailAddress}</p>
-                        <p>Telefono: {Employees[0].phoneNumber}</p>
-                        <p>Texto: {Employees[0].texto}</p>
-
-                        <br/>
-
-                        <p>Nombre: {Employees[1].preferredFullName}</p>
-                        <p>Correo: {Employees[1].emailAddress}</p>
-                        <p>Telefono: {Employees[1].phoneNumber}</p>
-                        <p>Texto: {Employees[1].texto}</p>
-                    </div>
+                {/* {Employees.map((data, key) => {
+                    return <h2 key={key}>{data.preferredFullName}</h2>;
+                })
+                } */}
+                <div  className='texto'>
+                    {mostrarTexto()}
+                </div>
                     
             </div>
         </div>

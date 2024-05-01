2e29927e02f85fe4e197b739bdc720e2f6315cbd
Michelle <michellegasca16@tectijuana>
2024-05-01 01:01:01 -0700

    navegación en la parte derecha

---
+++ src/ganchos/IrAlComponente.jsx
@@ -0,0 +1,18 @@
+import { useEffect, useRef, useState } from 'react';
+
+const useScrollTo = () => {
+  const ref = useRef(null);
+  const [shouldScrollTo, setShouldScrollTo] = useState(false);
+
+  useEffect(() => {
+    if (ref.current && shouldScrollTo) {
+      ref.current.scrollIntoView({ behavior: 'smooth' });
+      setShouldScrollTo(false);
+    }
+  }, [shouldScrollTo]);
+
+  return [ref, setShouldScrollTo];
+};
+
+export default useScrollTo;
+

edd582a038ebc4a0ba2173b87c6c146da4f28c6b
mich_iv <michelle.ivgo@gmail.com>
2024-07-09 15:25:51 -0700

    Primer versión terminada

---
+++ src/routes/secciones/students.jsx
@@ -0,0 +1,21 @@
+import React, { useEffect, useState } from 'react'
+import '../../estilos/Paginas.css';
+import { useLoaderData, useLocation } from 'react-router-dom';
+import {bd, collection, getDocs, doc, getDoc} from '../../../firebase.jsx';
+
+import MostrarTexto from '../../Components/MostrarTexto.jsx';
+
+export default function Route(){
+
+    return(
+        <>
+            <h1 className='titulos'>
+                Students
+            </h1>
+
+            <div  className='texto'>
+                <MostrarTexto/>
+            </div>
+        </>
+    )
+}

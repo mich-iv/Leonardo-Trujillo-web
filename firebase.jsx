79035041670281623540af325e8da7d1870b73ed
mich_iv <michelle.ivgo@gmail.com>
2025-02-24 22:07:29 -0800

    A VEEEEEEEER

--- firebase.jsx
+++ firebase.jsx
@@ -1,7 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from 'firebase/firestore';
-import { collection, addDoc, getDocs, getDoc, doc, orderBy, query, deleteDoc} from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_API_KEY,
